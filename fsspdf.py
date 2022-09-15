bf008bb98460d986d4070375a07387aa8ddf189c
Rekt77 <timtaeil@gmail.com>
2022-09-15 22:46:17 +0900

    기존 : 스크립트와 같은 폴더에 pdf저장, 변경 : files폴더에 저장

--- fsspdf.py
+++ fsspdf.py
@@ -95,7 +95,12 @@ if __name__ == "__main__":
             soup=BeautifulSoup(requests.get(new_url).text,'html.parser')
             pdfurl = soup.find("div",class_="file-list__set__item").a["href"]
             fname = soup.find("span",class_="name").text
-            fname = download_file("/".join((base_url,pdfurl)),fname)
+            
+            #폴더가 없을 시
+            if not os.path.exists("./files"):
+                os.mkdir("files")
+
+            fname = download_file("./files".join((base_url,pdfurl)),fname)
             if fname.split(".")[-1] == "pdf":
                 isContains = pdfExtractor(fname).flag
             else:
